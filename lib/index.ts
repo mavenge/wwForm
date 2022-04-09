90a726e5502f38b46b2ece438fb2a7b097d9b1f3
umige <84163649+mavenge@users.noreply.github.com>
2022-04-09 17:02:35 +0800

    :white_check_mark: pass array field unit test

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,9 @@
 import SchemaForm from './SchemaForm'
 import NumberField from './fields/NumberField.vue'
 import StringField from './fields/StringField.vue'
+import ArrayField from './fields/ArrayField'
+import Selection from './widgets/Selection'
 
 export default SchemaForm
 
-export { NumberField, StringField }
+export { NumberField, StringField, ArrayField, Selection }
---
+++ tests/unit/ArrayField.spec.ts
@@ -0,0 +1,77 @@
+import { mount } from '@vue/test-utils'
+
+import SchemaForm, {
+  ArrayField,
+  StringField,
+  NumberField,
+  Selection,
+} from '../../lib'
+
+describe('ArrayFiled', () => {
+  it('should render multi type field', async () => {
+
+    let value: any
+
+    const wrapper = mount(SchemaForm, {
+      props: {
+        schema: {
+          type: 'array',
+          items: [{ type: 'string' }, { type: 'number' }],
+        },
+        value,
+        onChange: (v: any) => { value = v },
+      },
+    })
+
+    const arr = wrapper.findComponent(ArrayField)
+    const str = arr.findComponent(StringField)
+    const num = arr.findComponent(NumberField)
+
+    await arr.props('onChange')([])
+
+    expect(str.exists()).toBeTruthy()
+    expect(num.exists()).toBeTruthy()
+  })
+
+  it('should render single type field', () => {
+    const wrapper = mount(SchemaForm, {
+      props: {
+        schema: {
+          type: 'array',
+          items: {
+            type: 'string',
+          },
+        },
+        value: ['1', '2'],
+        onChange: () => {},
+      },
+    })
+
+    const arr = wrapper.findComponent(ArrayField)
+    const strs = arr.findAllComponents(StringField)
+
+    expect(strs.length).toBe(2)
+    expect(strs[0].props('value')).toBe('1')
+  })
+
+  it('should render single type field with enum', () => {
+    const wrapper = mount(SchemaForm, {
+      props: {
+        schema: {
+          type: 'array',
+          items: {
+            type: 'string',
+            enum: ['1', '2'],
+          },
+        },
+        value: [],
+        onChange: () => {},
+      },
+    })
+
+    const arr = wrapper.findComponent(ArrayField)
+    const select = arr.findComponent(Selection)
+
+    expect(select.exists()).toBeTruthy()
+  })
+})
