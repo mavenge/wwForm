9b7af81205e9afeec9084dd7f67da7836e303de7
umige <84163649+mavenge@users.noreply.github.com>
2022-04-10 00:29:27 +0800

    :rocket: the first deployment

--- vue.config.js
+++ vue.config.js
@@ -4,14 +4,15 @@ const CircularDependencyPlugin = require('circular-dependency-plugin')
 const isLib = process.env.TYPE === 'lib'
 
 module.exports = {
-  configureWebpack(config) {
-    // console.log(config.plugins)
-  },
+  publicPath: process.env.TYPE === 'github.io' ? './' : '/',
+  // configureWebpack(config) {
+  //   console.log(config.plugins)
+  // },
   chainWebpack(config) {
+    // console.log('config.plugin', config.plugin) // config.plugin [Function: plugin]
     if (!isLib) {
       config.plugin('monaco').use(new MonacoWebpackPlugin())
     }
     config.plugin('circular').use(new CircularDependencyPlugin())
   },
-  pwa: {}
 }
